4aedeb3ffd2ee99b5e5edbab18a93159d80d28bf
indresh-007 <indresh209721@gmail.com>
2026-06-09 10:46:11 +0530

    Let's write code

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = "Namaste Node JS";
+
+var a = 10;
+var b = 20;
+
+// console.log(name);
+// console.log(a + b);
+
+// console.log(global);
+
+console.log(this); // Empty Object
+
+console.log(globalThis);
+
+console.log(globalThis === global); // true

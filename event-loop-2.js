7b2b78fc6fe1a838b26b56c5cd8edd34d58c89c9
indresh-007 <indresh209721@gmail.com>
2026-06-15 19:29:43 +0530

    working of event loop

---
+++ event-loop-2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const a = 100;
+
+setImmediate(() => console.log("setImmediate"));
+
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", () => {
+    console.log("File Reading CB");
+});
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+process.nextTick(() => console.log("process.nextTick"));
+
+function printA() {
+    console.log("a=", a);
+}
+
+printA();
+console.log("Last line of the file.");
+
+// output is ->
+/*
+a=100
+Last line of the file.
+process.nextTick
+Promise
+timer expired
+setImmediate
+File Reading CB
+*/
